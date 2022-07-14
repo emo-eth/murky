498078b778de45b066b79b1acbb87c54f08f23ab
James Wenzel <james.wenzel@opensea.io>
2022-07-13 19:39:10 -0700

    port core logic of getProof and hashLevel to assembly

--- src/common/MurkyBase.sol
+++ src/common/MurkyBase.sol
@@ -2,27 +2,43 @@
 pragma solidity ^0.8.4;
 
 abstract contract MurkyBase {
+    uint136 constant _2_128 = 2**128;
+    uint72 constant _2_64 = 2**64;
+    uint40 constant _2_32 = 2**32;
+    uint24 constant _2_16 = 2**16;
+    uint16 constant _2_8 = 2**8;
+    uint8 constant _2_4 = 2**4;
+    uint8 constant _2_2 = 2**2;
+    uint8 constant _2_1 = 2**1;
+
     /***************
-    * CONSTRUCTOR *
-    ***************/
+     * CONSTRUCTOR *
+     ***************/
     constructor() {}
 
     /********************
-    * VIRTUAL HASHING FUNCTIONS *
-    ********************/
-    function hashLeafPairs(bytes32 left, bytes32 right) public pure virtual returns (bytes32 _hash);
-
+     * VIRTUAL HASHING FUNCTIONS *
+     ********************/
+    function hashLeafPairs(bytes32 left, bytes32 right)
+        public
+        pure
+        virtual
+        returns (bytes32 _hash);
 
     /**********************
-    * PROOF VERIFICATION *
-    **********************/
-    
-    function verifyProof(bytes32 root, bytes32[] memory proof, bytes32 valueToProve) external pure returns (bool) {
+     * PROOF VERIFICATION *
+     **********************/
+
+    function verifyProof(
+        bytes32 root,
+        bytes32[] calldata proof,
+        bytes32 valueToProve
+    ) external pure returns (bool) {
         // proof length must be less than max array size
         bytes32 rollingHash = valueToProve;
         uint256 length = proof.length;
         unchecked {
-            for(uint i = 0; i < length; ++i){
+            for (uint256 i = 0; i < length; ++i) {
                 rollingHash = hashLeafPairs(rollingHash, proof[i]);
             }
         }
@@ -30,160 +46,183 @@ abstract contract MurkyBase {
     }
 
     /********************
-    * PROOF GENERATION *
-    ********************/
+     * PROOF GENERATION *
+     ********************/
 
     function getRoot(bytes32[] memory data) public pure returns (bytes32) {
         require(data.length > 1, "won't generate root for single leaf");
-        while(data.length > 1) {
+        while (data.length > 1) {
             data = hashLevel(data);
         }
         return data[0];
     }
 
-    function getProof(bytes32[] memory data, uint256 node) public pure returns (bytes32[] memory) {
+    function getProof(bytes32[] memory data, uint256 node)
+        public
+        pure
+        returns (bytes32[] memory result)
+    {
         require(data.length > 1, "won't generate proof for single leaf");
-        // The size of the proof is equal to the ceiling of log2(numLeaves) 
-        bytes32[] memory result = new bytes32[](log2ceilBitMagic(data.length));
-        uint256 pos = 0;
-
+        // The size of the proof is equal to the ceiling of log2(numLeaves)
         // Two overflow risks: node, pos
         // node: max array size is 2**256-1. Largest index in the array will be 1 less than that. Also,
-           // for dynamic arrays, size is limited to 2**64-1
+        // for dynamic arrays, size is limited to 2**64-1
         // pos: pos is bounded by log2(data.length), which should be less than type(uint256).max
-        while(data.length > 1) {
-            unchecked {
-                if(node & 0x1 == 1) {
-                    result[pos] = data[node - 1];
-                } 
-                else if (node + 1 == data.length) {
-                    result[pos] = bytes32(0);  
-                } 
-                else {
-                    result[pos] = data[node + 1];
+        uint256 resultIndexPtr;
+        uint256 length;
+        assembly {
+            result := mload(0x40)
+            resultIndexPtr := add(0x20, result)
+        }
+        while (data.length > 1) {
+            assembly {
+                let oddNodeIndex := and(node, 1)
+                let lastNodeIndex := eq(mload(data), add(1, node))
+                let switchVal := or(shl(1, lastNodeIndex), oddNodeIndex)
+                switch switchVal
+                // neither odd nor last
+                case 0 {
+                    // get pointer to result[node+1] by adding 2 to node and multiplying by 0x20
+                    // to account for the fact that result points to array length, not first index
+                    mstore(
+                        resultIndexPtr,
+                        mload(add(data, mul(0x20, add(2, node))))
+                    )
                 }
-                ++pos;
-                node /= 2;
+                // node is last
+                case 2 {
+                    mstore(resultIndexPtr, 0)
+                }
+                // node is odd (and possibly also last)
+                default {
+                    mstore(resultIndexPtr, mload(add(data, mul(0x20, node))))
+                }
+                resultIndexPtr := add(0x20, resultIndexPtr)
+                node := div(node, 2)
+                length := add(1, length)
             }
             data = hashLevel(data);
+        }
+        assembly {
+            // TODO: test length and result free mem ptr get set correctly
+            mstore(result, length)
+            mstore(0x40, resultIndexPtr)
         }
         return result;
     }
 
     ///@dev function is private to prevent unsafe data from being passed
-    function hashLevel(bytes32[] memory data) private pure returns (bytes32[] memory) {
-        bytes32[] memory result;
+    function hashLevel(bytes32[] memory data)
+        private
+        pure
+        returns (bytes32[] memory result)
+    {
+        // bytes32[] memory result;
 
         // Function is private, and all internal callers check that data.length >=2.
         // Underflow is not possible as lowest possible value for data/result index is 1
-        // overflow should be safe as length is / 2 always. 
-        unchecked {
-            uint256 length = data.length;
-            if (length & 0x1 == 1){
-                result = new bytes32[](length / 2 + 1);
-                result[result.length - 1] = hashLeafPairs(data[length - 1], bytes32(0));
-            } else {
-                result = new bytes32[](length / 2);
+        // overflow should be safe as length is / 2 always.
+        uint256 length;
+        uint256 newLength;
+        uint256 resultIndexPointer;
+        uint256 dataIndexPointer;
+        uint256 stopIteration;
+        bool hashLast;
+        assembly {
+            result := data
+            length := mload(data)
+            switch and(length, 1)
+            case 1 {
+                newLength := add(1, div(length, 2))
+                hashLast := 1
+                // todo: hash last node with zero
+            }
+            default {
+                newLength := div(length, 2)
+            }
+            mstore(data, newLength)
+            resultIndexPointer := add(0x20, data)
+            dataIndexPointer := resultIndexPointer
+            // stop iterating over for loop at length-1
+            // todo: does this make sense
+            stopIteration := add(data, mul(length, 0x20))
         }
-        // pos is upper bounded by data.length / 2, so safe even if array is at max size
-            uint256 pos = 0;
-            for (uint256 i = 0; i < length-1; i+=2){
-                result[pos] = hashLeafPairs(data[i], data[i+1]);
-                ++pos;
+        for (; dataIndexPointer < stopIteration; ) {
+            bytes32 data1;
+            bytes32 data2;
+            assembly {
+                data1 := mload(dataIndexPointer)
+                data2 := mload(add(dataIndexPointer, 0x20))
+            }
+            bytes32 hashedPair = hashLeafPairs(data1, data2);
+            assembly {
+                mstore(resultIndexPointer, hashedPair)
+                resultIndexPointer := add(0x20, resultIndexPointer)
+                dataIndexPointer := add(0x40, dataIndexPointer)
             }
         }
-        return result;
+        if (hashLast) {
+            bytes32 data1;
+            assembly {
+                data1 := mload(add(data, mul(0x20, length)))
+            }
+            bytes32 hashedPair = hashLeafPairs(data1, bytes32(0));
+            assembly {
+                mstore(resultIndexPointer, hashedPair)
+            }
+        }
     }
 
     /******************
-    * MATH "LIBRARY" *
-    ******************/
-    
-    /// @dev  Note that x is assumed > 0
-    function log2ceil(uint256 x) public pure returns (uint256) {
-        uint256 ceil = 0;
-        uint pOf2;
-        // If x is a power of 2, then this function will return a ceiling
-        // that is 1 greater than the actual ceiling. So we need to check if
-        // x is a power of 2, and subtract one from ceil if so. 
-        assembly {
-            // we check by seeing if x == (~x + 1) & x. This applies a mask
-            // to find the lowest set bit of x and then checks it for equality
-            // with x. If they are equal, then x is a power of 2.
-
-            /* Example
-                x has single bit set
-                x := 0000_1000
-                (~x + 1) = (1111_0111) + 1 = 1111_1000
-                (1111_1000 & 0000_1000) = 0000_1000 == x
-
-                x has multiple bits set
-                x := 1001_0010
-                (~x + 1) = (0110_1101 + 1) = 0110_1110
-                (0110_1110 & x) = 0000_0010 != x
-            */
-
-            // we do some assembly magic to treat the bool as an integer later on
-            pOf2 := eq(and(add(not(x), 1), x), x)
-        }
-        
-        // if x == type(uint256).max, than ceil is capped at 256
-        // if x == 0, then pO2 == 0, so ceil won't underflow
-        unchecked {
-            while( x > 0) {
-                x >>= 1;
-                ceil++;
-            }
-            ceil -= pOf2; // see above
-        }
-        return ceil;
-    }
+     * MATH "LIBRARY" *
+     ******************/
 
     /// Original bitmagic adapted from https://github.com/paulrberg/prb-math/blob/main/contracts/PRBMath.sol
     /// @dev Note that x assumed > 1
-    function log2ceilBitMagic(uint256 x) public pure returns (uint256){
-        if (x <= 1) {
-            return 0;
-        }
-        uint256 msb = 0;
-        uint256 _x = x;
-        if (x >= 2**128) {
-            x >>= 128;
-            msb += 128;
-        }
-        if (x >= 2**64) {
-            x >>= 64;
-            msb += 64;
-        }
-        if (x >= 2**32) {
-            x >>= 32;
-            msb += 32;
-        }
-        if (x >= 2**16) {
-            x >>= 16;
-            msb += 16;
-        }
-        if (x >= 2**8) {
-            x >>= 8;
-            msb += 8;
-        }
-        if (x >= 2**4) {
-            x >>= 4;
-            msb += 4;
-        }
-        if (x >= 2**2) {
-            x >>= 2;
-            msb += 2;
-        }
-        if (x >= 2**1) {
-            msb += 1;
-        }
-
-        uint256 lsb = (~_x + 1) & _x;
-        if ((lsb == _x) && (msb > 0)) {
-            return msb;
-        } else {
-            return msb + 1;
+    function log2ceilBitMagic(uint256 x)
+        public
+        pure
+        returns (uint256 mostSignificantBit)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let xCopy := x
+            if iszero(lt(x, _2_128)) {
+                x := shr(128, x)
+                mostSignificantBit := add(mostSignificantBit, 128)
+            }
+            if iszero(lt(x, _2_64)) {
+                x := shr(64, x)
+                mostSignificantBit := add(mostSignificantBit, 64)
+            }
+            if iszero(lt(x, _2_32)) {
+                x := shr(32, x)
+                mostSignificantBit := add(mostSignificantBit, 32)
+            }
+            if iszero(lt(x, _2_16)) {
+                x := shr(16, x)
+                mostSignificantBit := add(mostSignificantBit, 16)
+            }
+            if iszero(lt(x, _2_8)) {
+                x := shr(8, x)
+                mostSignificantBit := add(mostSignificantBit, 8)
+            }
+            if iszero(lt(x, _2_4)) {
+                x := shr(4, x)
+                mostSignificantBit := add(mostSignificantBit, 4)
+            }
+            if iszero(lt(x, _2_2)) {
+                x := shr(2, x)
+                mostSignificantBit := add(mostSignificantBit, 2)
+            }
+            if iszero(lt(x, _2_1)) {
+                // No need to shift x any more.
+                mostSignificantBit := add(mostSignificantBit, 1)
+            }
+            let lsb := and(add(1, not(xCopy)), xCopy)
+            if and(iszero(eq(lsb, xCopy)), gt(xCopy, 0)) {
+                mostSignificantBit := add(mostSignificantBit, 1)
+            }
         }
     }
 }
--- src/test/StandardInput.t.sol
+++ src/test/StandardInput.t.sol
@@ -14,51 +14,78 @@ contract StandardizedInputTest is Test {
         string[] memory inputs = new string[](2);
         inputs[0] = "cat";
         inputs[1] = "src/test/standard_data/StandardInput.txt";
-        bytes memory result =  vm.ffi(inputs);
+        bytes memory result = vm.ffi(inputs);
         data = abi.decode(result, (bytes32[100]));
         x = new Xorkle();
         m = new Merkle();
     }
-    
-    function testXorkleGenerateProofStandard() public view {
-        bytes32[] memory _data = _getData(); 
-        for (uint i = 0; i < leaves.length; ++i) {
-            x.getProof(_data, leaves[i]);
-        }
 
+    function testXorkleGenerateProofStandard() public view {
+        Xorkle _x = x;
+        bytes32[] memory _data = _getData();
+        uint256[] memory _leaves = _getLeaves();
+        uint256 leavesLength = _leaves.length;
+        for (uint256 i = 0; i < leavesLength; ++i) {
+            _x.getProof(_data, _leaves[i]);
+        }
     }
 
     function testMerkleGenerateProofStandard() public view {
+        Merkle _m = m;
         bytes32[] memory _data = _getData();
-        for(uint i = 0; i < leaves.length; ++i) {
-            m.getProof(_data, leaves[i]);
+        uint256[] memory _leaves = _getLeaves();
+        uint256 leavesLength = _leaves.length;
+
+        for (uint256 i = 0; i < leavesLength; ++i) {
+            _m.getProof(_data, _leaves[i]);
         }
     }
 
     function testXorkleVerifyProofStandard() public {
+        Xorkle _x = x;
         bytes32[] memory _data = _getData();
-        bytes32 root = x.getRoot(_data);
-        for (uint i = 0; i < leaves.length; ++i) {
-            bytes32[] memory proof = x.getProof(_data, leaves[i]);
-            assertTrue(x.verifyProof(root, proof, _data[leaves[i]]));
+        uint256[] memory _leaves = _getLeaves();
+        bytes32 root = _x.getRoot(_data);
+        uint256 leavesLength = _leaves.length;
+        for (uint256 i = 0; i < leavesLength; ++i) {
+            bytes32[] memory proof = x.getProof(_data, _leaves[i]);
+            assertTrue(_x.verifyProof(root, proof, _data[_leaves[i]]));
         }
     }
 
     function testMerkleVerifyProofStandard() public {
+        Merkle _m = m;
         bytes32[] memory _data = _getData();
-        bytes32 root = m.getRoot(_data);
-        for (uint i = 0; i < leaves.length; ++i) {
-            bytes32[] memory proof = m.getProof(_data, leaves[i]);
-            assertTrue(m.verifyProof(root, proof, _data[leaves[i]]));
+        uint256[] memory _leaves = _getLeaves();
+        bytes32 root = _m.getRoot(_data);
+        uint256 leavesLength = _leaves.length;
+        for (uint256 i = 0; i < leavesLength; ++i) {
+            bytes32[] memory proof = _m.getProof(_data, _leaves[i]);
+            assertTrue(_m.verifyProof(root, proof, _data[_leaves[i]]));
         }
+    }
+
+    function testStandardRetrievalGasReference() public view {
+        Merkle _m = m;
+        bytes32[] memory _data = _getData();
+        uint256[] memory _leaves = _getLeaves();
     }
 
     function _getData() public view returns (bytes32[] memory) {
         bytes32[] memory _data = new bytes32[](data.length);
-        uint length = data.length;
-        for (uint i = 0; i < length; ++i) {
+        uint256 length = data.length;
+        for (uint256 i = 0; i < length; ++i) {
             _data[i] = data[i];
         }
         return _data;
+    }
+
+    function _getLeaves() public view returns (uint256[] memory) {
+        uint256[] memory _leaves = new uint256[](leaves.length);
+        uint256 length = leaves.length;
+        for (uint256 i = 0; i < length; ++i) {
+            _leaves[i] = leaves[i];
+        }
+        return _leaves;
     }
 }
